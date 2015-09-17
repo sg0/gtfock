9fef7161892442ee7780da13007f9df485ac8f8c
Sayan Ghosh <zsayanz@gmail.com>
2015-09-17 13:43:52 -0500

    forgot to add new elem.h file

---
+++ pfock/elem.h
@@ -0,0 +1,10 @@
+#ifndef __ELEM_H__
+#define __ELEM_H__
+
+/* global El::GlobalArrays instances */
+#include <El.h>
+extern ElGlobalArrays_i eliga;
+extern ElGlobalArrays_s elsga;
+extern ElGlobalArrays_d eldga;
+
+#endif
